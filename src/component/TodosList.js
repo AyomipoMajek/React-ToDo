96bd6798a8d5a636bf8abe8eb48f42e83ecf0af5
Ayomipo Majek <ayomipomajek@gmail.com>
2023-02-21 15:30:19 +0100

    project done to an extent

---
+++ src/component/TodosList.js
@@ -0,0 +1,12 @@
+import TodoItem from './TodoItem';
+
+const TodosList = ({ todosProps, handleChange, delTodo, setUpdate }) => {
+    <ul>
+    {todosProps.map((todo) => (
+      <TodoItem key={todo.id} itemProp={todo} setTodos={setTodos} delTodo={delTodo}
+      setUpdate={setUpdate}
+      />
+    ))}
+  </ul>
+};
+export default TodosList;
